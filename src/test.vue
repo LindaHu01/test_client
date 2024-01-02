3324340245828c89f6080d0a6197c0263d32b09e
LindaHu01 <138744236+LindaHu01@users.noreply.github.com>
2024-01-02 15:55:56 +0800

    Update test.vue
    888

--- src/test.vue
+++ src/test.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
       <img alt="Vue logo" src="./assets/logo.png">
-      <el-button type="primary">添加了88</el-button>
+      <el-button type="primary">添加了888888</el-button>
       <HelloWorld msg="Welcome to Your Vue.js App"/>
     </div>
   </template>
