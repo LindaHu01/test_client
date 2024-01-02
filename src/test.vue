ebe381033891261262660a45e8dfd6e209a69ea6
LindaHu01 <138744236+LindaHu01@users.noreply.github.com>
2024-01-02 15:52:59 +0800

    Update test.vue
    77

--- src/test.vue
+++ src/test.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
       <img alt="Vue logo" src="./assets/logo.png">
-      <el-button type="primary">添加了个测试页面</el-button>
+      <el-button type="primary">添加了</el-button>
       <HelloWorld msg="Welcome to Your Vue.js App"/>
     </div>
   </template>
