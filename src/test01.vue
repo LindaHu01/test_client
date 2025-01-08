0475646e9544b31c068058c2fa4ff62a4a4829e3
LindaHu01 <138744236+LindaHu01@users.noreply.github.com>
2025-01-08 16:46:15 +0800

    Update test01.vue

--- src/test01.vue
+++ src/test01.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
       <img alt="Vue logo" src="./assets/logo.png">
-      <el-button type="primary">个88i1</el-button>
+      <el-button type="primary">个8441</el-button>
       <HelloWorld msg="Welcome to Your Vue.js App"/>
     </div>
   </template>
